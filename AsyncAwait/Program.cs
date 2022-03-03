0e86e240adee3bef9617813296ea843a910da1ae
hinal.patel <hinal.patel@1R-79>
2022-03-03 15:00:54 +0530

    Add project files.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            high();
+            simple();
+
+            MyTask myTask = new MyTask();
+            myTask.high().Wait();
+
+            Console.ReadKey();
+
+        }
+
+        private static async void high()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Thread.Sleep(5000);
+                    Console.WriteLine("\n Method do high");
+
+                    Task.Delay(1000).Wait();
+                }
+
+            });
+        }
+
+        private static void simple()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("\n Method do simple");
+
+                Task.Delay(1000).Wait();
+            }
+        }
+
+        class MyTask
+        {
+            public async Task high()
+            {
+                int a = await doSomethingAsync(5);
+                return;
+            }
+            private async Task<int> doSomethingAsync(int v)
+            {
+                return await Task.Run(() =>
+                {
+
+                    Thread.Sleep(1000);
+                    Console.WriteLine("\n doSomethingAsync");
+                    Console.Write("\n " + (v * 2));
+                    return v * 2;
+
+                });
+            }
+        }
+    }
+}
